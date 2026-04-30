d3ddf763f999d2c9644a946acf3941bbdbe2fcbb
Andre Pham <andrepham28@gmail.com>
2026-04-29 23:57:20 -0700

    Set up main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
+#include "../include/List.h"
+using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./hw <input_file>" << std::endl;
+        return 1;
+    }
+
+    string input_file = "words.txt";
+    
+    cout << "Running list benchmarks with input file: "
+         << input_file << endl;
+
+    measure_lists(input_file);
+
     return 0;
 }
