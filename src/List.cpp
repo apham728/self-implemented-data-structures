f45224762a05ddd72877bca54f695d9edede1029
Andre Pham <andrepham28@gmail.com>
2026-04-29 21:59:46 -0700

    Implemented insert(), remove(), erase(), print() and destructor for ArrayList

--- src/List.cpp
+++ src/List.cpp
@@ -26,10 +26,46 @@ bool ArrayList::find(const string & word) {
     return find_index(word) != -1;
 }
 
+void ArrayList::insert(const string & word) {
+    // check if inserting into full list
+    if (is_full()) {
+        error(word, "Trying to insert into a full list"); return;
+    }
+
+    // don't have to check if the inserted word will be duplicate so just insert
+    buf[size] = word;
+    size++;
+}
+
+void ArrayList::remove(const string & word) {
+    int index = find_index(word);
+
+    // check if word exists
+    if (index == -1) { error(word, "Remove a word that doesn't exist in the list"); return; }
+
+    // replace element at index with last word and decrement size
+    buf[index] = buf[size-1];
+    size--;
+}
+
 bool ArrayList::is_empty() {
     return size == 0;
 }
 
 bool ArrayList::is_full() {
     return size == capacity;
+}
+
+void ArrayList::erase() {
+    size = 0;
+}
+
+void ArrayList::print(ostream & out) {
+    for (int i = 0; i < size; i++) {
+        out << buf[i] << endl;
+    }
+}
+
+ArrayList::~ArrayList() {
+    delete[] buf;
 }
